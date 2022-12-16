8cee665004ce6031f9b06c38917aa0f7fe064418
Marion Lang <langma@gmail.com>
2022-12-16 14:19:28 -0600

    add script for subtree update

---
+++ subtree_bootstrap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git subtree add --prefix _labs/lab0 git@github.com:comp251/comp251-lab0.git main --squash
--- subtree_update.sh
+++ subtree_update.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
 git subtree pull --prefix _labs/lab0 git@github.com:comp251/comp251-lab0.git main --squash
