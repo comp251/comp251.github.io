3a33122db10e972c7fe79c02bcd9548142798678
Marion Lang <langma@gmail.com>
2023-04-16 18:37:27 -0500

    lab 5

---
+++ _labs/lab5.md
@@ -0,0 +1,13 @@
+---
+layout: default
+title: Lab 5 (due 5/1)
+nav_order: 5
+---
+
+Below is the handout for Lab 5. [Accept the
+assignment](https://classroom.github.com/a/YM8O2m4N) on GitHub Classroom to get
+started!
+
+# Lab 5 Handout
+
+{% include_relative lab5/README.md %}
--- submodule_bootstrap.sh
+++ submodule_bootstrap.sh
@@ -5,3 +5,4 @@ git submodule add https://github.com/comp251/comp251-lab1.git _labs/lab1
 git submodule add https://github.com/comp251/comp251-lab2.git _labs/lab2
 git submodule add https://github.com/comp251/comp251-lab3.git _labs/lab3
 git submodule add https://github.com/comp251/comp251-lab4.git _labs/lab4
+git submodule add https://github.com/comp251/comp251-lab5.git _labs/lab5
